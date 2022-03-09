c6d1ffa3325dc1de21b9a399ebcaa29585fe8db5
Mohammad Wasikuzzaman <wasik0003@gmail.com>
2022-03-10 02:07:38 +0600

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -13,12 +13,6 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'food-order');
 
-//define('LOCALHOST', 'sql303.unaux.com:3306');
-//define('DB_USERNAME', 'unaux_30756807');
-//define('DB_PASSWORD', 'udhx76r8');
-//define('DB_NAME', 'unaux_30756807_food_order');
-
-
 
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());
     
